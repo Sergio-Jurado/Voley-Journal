caeb71c12352edb54c315cf9361742c7aeda9f3f
Sergio Jurado <juradotrillosergio1207@gmail.com>
2025-03-29 17:14:29 +0100

    Back-end changes

--- Back-end/server.js
+++ Back-end/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const path = require('path');
 const userRoutes = require('./routes/userRoutes');
 const newsRoutes = require('./routes/newsRoutes');
 const teamRoutes = require('./routes/teamRoutes');
@@ -15,6 +16,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use('/media', express.static(path.join(__dirname, 'media'))); // Middleware para servir archivos estáticos
 app.use(cors());
 app.use(express.json());
 
